ee1193182b9cb337f33763feb40cebc78ebbb1f9
Oskyce <OSCARCENTRO@GMAIL.COM>
2022-05-17 17:49:29 +0200

    aaa

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'sphinx.ext.duration',
+    'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
